b8a7b396fa48d7af8c23cb360cba64104441c6d3
SafiraMiya <135469170+SafiraMiya@users.noreply.github.com>
2026-01-14 19:02:54 -0300

    missing files

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const playerRoutes = require('./routes/playerRoutes');
 const cardRoutes = require('./routes/cardRoutes');
+const scoringHistoryRoutes = require('./routes/scoringHistoryRoutes'); 
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 // Definição de Rotas
 app.use('/api/players', playerRoutes);
 app.use('/api/cards', cardRoutes);
+app.use('/api/scoring-history', scoringHistoryRoutes);
 
 // Exportamos o 'app' para que o server.js ou arquivos de teste possam usá-lo
 module.exports = app;
