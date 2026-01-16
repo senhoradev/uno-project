df8eba9d91e1aa489d57431b888fa553d19cea8b
Grazielle Nascimento <grazigrazielle454@gmail.com>
2026-01-16 07:23:22 -0300

    feat: add game routes to the application

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-// src/app.js
 const express = require('express');
 const playerRoutes = require('./routes/playerRoutes');
 const cardRoutes = require('./routes/cardRoutes');
-const scoringHistoryRoutes = require('./routes/scoringHistoryRoutes'); 
+const scoringHistoryRoutes = require('./routes/scoringHistoryRoutes');
+const gameRoutes = require('./routes/gameRoutes');
 
 const app = express();
 
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use('/api/players', playerRoutes);
 app.use('/api/cards', cardRoutes);
 app.use('/api/scoring-history', scoringHistoryRoutes);
+app.use('/api/games', gameRoutes);
 
 // Exportamos o 'app' para que o server.js ou arquivos de teste possam usá-lo
 module.exports = app;
