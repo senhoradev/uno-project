68526b0402d6a5dc47084537e2a019146cb9d700
nvitor <Vitor.Pereira0724@jala.university>
2026-01-26 15:28:50 -0300

    dto

--- src/DTO/Response/PlayerResponseDTO.js
+++ src/DTO/Response/PlayerResponseDTO.js
@@ -1,9 +1,6 @@
 class PlayerResponseDTO {
     constructor({ username, name}) {
         this.username = username;
-        this.email = email;
-        this.password = password;
-        this.age = age;
         this.name = name;
     }
 }
